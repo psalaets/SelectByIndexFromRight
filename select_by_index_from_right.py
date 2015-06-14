7ff2c21361f3aa5c621175572a3e2f68c6e2159d
Paul Salaets <psalaets@gmail.com>
2015-06-14 10:47:24 -0400

    here goes

---
+++ select_by_index_from_right.py
@@ -0,0 +1,15 @@
+import sublime_plugin
+
+class SelectByIndexFromRightCommand(sublime_plugin.WindowCommand):
+    def run(self, index):
+        group = self.window.active_group()
+        views = self.window.views_in_group(group)
+        
+        if index >= -len(views) and index < 0:
+            view = views[index]
+            self.window.focus_view(view)
+    
+    def is_enabled(self, index):
+        group = self.window.active_group()
+        views = self.window.views_in_group(group)
+        return len(views) > 0
